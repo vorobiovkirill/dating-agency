12210f114e0b6e8b5e362de648b49bdd5cbe0a7d
Кирилл Воробьев <mishkacod@gmail.com>
2016-10-26 17:43:10 +0300

    update 26/10/16 profile-function redesign

--- src/js/common.js
+++ src/js/common.js
@@ -47,22 +47,45 @@ $(function() {
 	// Обьявляем вызов Мобильного меню
 	// Документация: http://dimsemenov.com/plugins/magnific-popup/documentation.html
 
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
+	$('.profile-avatar-wrapper').magnificPopup({
+		items: [
+		{
+			src: 'img/anketa-images/telka-1.jpg'
+		},
+		{
+			src: 'img/anketa-images/telka-2.jpg'
+		},
+		{
+			src: 'img/anketa-images/telka-3.jpg'
+		},
+		{
+			src: 'img/anketa-images/telka-4.jpg'
+		},
+		{
+			src: 'img/anketa-images/telka-5.jpg'
+		}
+		],
+
 		gallery: {
 			enabled: true,
 			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+			tPrev: 'Previous (Left arrow key)', // title for left button
+			tNext: 'Next (Right arrow key)', // title for right button
+			tCounter: '<span class="mfp-counter">%curr% of %total%</span>', // markup of counter
+			preload: [0,2] // Will preload 0 - before current, and 1 after the current image
 		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-			}
-		}
+
+		type: 'image',
+
+		preloader: true,
+
+		tLoading: 'Loading image #%curr%...',
+
+		mainClass: 'mfp-img-mobile'
+
+
 	});
 
 
