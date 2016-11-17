d48b2e9018f3645ec831487ca1437d362ffc4d78
Кирилл Воробьев <mishkacod@gmail.com>
2016-11-17 16:18:36 +0200

    update 16/11/16

--- src/js/common.js
+++ src/js/common.js
@@ -53,31 +53,24 @@ $(function() {
 
 		acc[i].onclick = function() {
 
+			var active = document.querySelector(".accordion.active");
+
+			if (active && active != this) {
+
+				active.classList.remove("active");
+				active.nextElementSibling.classList.remove("show");
+
+			}
+
 			this.classList.toggle("active");
+
 			this.nextElementSibling.classList.toggle("show");
-			this.nextElementSibling.classList.toggle("accordionIn");
+
+			// this.nextElementSibling.classList.toggle("accordionIn");
 
 		}
 
 	}
-
-/* FOR TEST
-* https://jsfiddle.net/arunpjohny/bcb7kh2y/
-*/
-	// var acc = document.getElementsByClassName("accordion");
-	// var i;
-
-	// for (i = 0; i < acc.length; i++) {
-	// 	acc[i].onclick = function() {
-	// 		var active = document.querySelector(".accordion.active");
-	// 		if (active && active != this) {
-	// 			active.classList.remove("active");
-	// 			active.nextElementSibling.classList.remove("show");
-	// 		}
-	// 		this.classList.toggle("active");
-	// 		this.nextElementSibling.classList.toggle("show");
-	// 	}
-	// }
 
 
 
